e1f3859217e809aea656b6d5bad4969ebcfc1c8d
Yw <1115348410@qq.com>
2019-07-22 14:51:24 +0800

    验证提交后的代码

---
+++ structure/src/test/java/algorithm/ExhaustionTest.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+import org.junit.Test;
+
+/**
+ * 递推算法 单元测试
+ * @author X
+ *
+ */
+public class ExhaustionTest {
+
+	@Test
+	public void testExhaustion()
+	{
+		Exhaustion exhaustion = new Exhaustion();
+		exhaustion.printCount(23, 56);
+	}
+}
